4aa694a8f3bb1350b1c6ff204ee70b1af7a624f4
Krishna Vishwakarma <krishna23712@gmail.com>
2020-04-13 19:02:59 +0530

    Search bar and Download Button Added

--- src/component/Images.js
+++ src/component/Images.js
@@ -1,47 +1,103 @@
 import React, { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faFileDownload } from "@fortawesome/free-solid-svg-icons";
 
 function Images() {
   const key = "14211217-1220ec0a2f19970f0350089d2";
+  let controller = new AbortController();
 
   const [page, setPage] = useState(1);
   const [images, setImages] = useState([]);
+  const [search, setSearch] = useState("");
 
-  const fetchImages = async () => {
-    const response = await fetch(
-      `https://pixabay.com/api/?key=${key}&q=beautiful&page=${page}`
-    );
-    const data = await response.json();
-    setImages(data.hits);
+  const fetchImages = () => {
+    controller.abort();
+    console.log(controller);
+
+    controller = new AbortController();
+    let { signal } = controller;
+
+    fetch(`https://pixabay.com/api/?key=${key}&q=${search}&page=${page}`, {
+      signal,
+    })
+      .then((data) => data.json())
+      .then((data) => {
+        setImages(data.hits);
+        console.log(data.hits);
+      });
+
     window.scrollTo(0, 0);
   };
 
   useEffect(() => {
     fetchImages();
-  }, [page]);
+  }, [page, search]);
 
-  const handleClick = e => {
+  const handleClick = (e) => {
     if ((e === -1 && page > 0) || e === 1) setPage(page + e);
   };
 
-  return (
-    <section className="body">
-      <ul className="imageList">
-        {images.map(image => (
-          <li key={image.id}>
-            <img src={image.webformatURL} alt={image.user_id} />
-          </li>
-        ))}
-      </ul>
+  const handleChange = (e) => {
+    console.log(e.target.value);
+    if (search !== e.target.value) {
+      setSearch(e.target.value);
+    }
+    console.log(search);
+  };
 
+  return (
+    <React.Fragment>
+      <label className="search">
+        {/* Add beautifull button to search content */}
+        <FontAwesomeIcon icon={faSearch} className="searchIcon" />
+        <input
+          type="text"
+          className="searchBar"
+          value={search}
+          onChange={handleChange}
+        />
+        <div className="searchBack"></div>
+      </label>
+
+      <section className="body">
+        {images.map((image) => (
+          <div
+            key={image.id}
+            className={`contains ${
+              image.id % 8 === 0
+                ? "hori"
+                : image.id % 6 === 0
+                ? "vert"
+                : image.id % 4 === 0
+                ? "big"
+                : ""
+            }`}
+          >
+            <img src={image.webformatURL} alt={image.user_id} />
+            <a
+              href="krishnaTheGreat"
+              className="download"
+              download={image.webformatURL}
+            >
+              <FontAwesomeIcon icon={faFileDownload} className="downloadIcon" />
+            </a>
+          </div>
+        ))}
+      </section>
       <div className="control">
-        <div className="left" onClick={() => handleClick(-1)}>
-          Prev
-        </div>
-        <div className="right" onClick={() => handleClick(1)}>
-          Next
-        </div>
+        <button
+          className="btn"
+          onClick={() => handleClick(-1)}
+          style={{ visibility: `${page === 1 ? "hidden" : "visible"}` }}
+        >
+          {`Page ${page > 1 ? page - 1 : "N/A"}`}
+        </button>
+        <div className="mid"> - {`Page ${page}`} - </div>
+        <button className="btn" onClick={() => handleClick(1)}>
+          {`Page ${page + 1}`}
+        </button>
       </div>
-    </section>
+    </React.Fragment>
   );
 }
 
